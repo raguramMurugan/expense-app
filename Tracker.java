9aef613c756dced8fa009a563d2ff842cfdf645d
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-21 19:15:04 +0530

    updated
    class name updated,
    expense Tracker method completed

--- Tracker.java
+++ Tracker.java
@@ -1,7 +1,6 @@
 package com.hubino.expensetrackingapp.controller;
-
-import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,58 +13,81 @@ import com.hubino.expensetrackingapp.entity.ExpenseTracker;
 import com.hubino.expensetrackingapp.entity.User;
 import com.hubino.expensetrackingapp.service.ExpenseService;
 import com.hubino.expensetrackingapp.service.UserService;
-import com.hubino.expensetrackingapp.service.UserServiceImpl;
 
 @RestController
-public class Tracker {
+public class ExpenseTrackerController {
 	
+	private static final Logger log=LoggerFactory.getLogger(ExpenseTrackerController.class);
+	
+	@Autowired
 	private UserService userService;
 	
+	@Autowired
 	private ExpenseService expenseService;
-	
-	public Tracker(ExpenseService expenseService)
-	{
-		this.expenseService=expenseService;
-	}
-	
-	public Tracker(UserServiceImpl userService)
-	{
-		this.userService=userService;
-	}
-	
-	@PostMapping("/signIn")
-	public String signIn(@RequestBody SignIn signin)
-	{
-		User user=new User(signin.getUsername(), signin.getPassword());
-		Integer userInDb=userService.checkCustomerinDb(signin);
-		if(userInDb!=null)
+		
+		/**
+		 * 
+		 * @param signin
+		 * @return
+		 */
+		
+		@PostMapping("/signIn")
+		public String signIn(@RequestBody SignIn signin)
 		{
-			user.setUserId(userInDb);
-			return "Account already exists, Kindly Login with your Username and Password";
+			
+			User user=new User(signin.getUsername(), signin.getPassword());
+			Integer userInDb=userService.checkCustomerinDb(signin);
+			log.info("checking user present in Database"+ userInDb);
+			
+			if(userInDb!=null)
+			{
+				
+				user.setUserId(userInDb);
+				log.info("User already present in Db with userID:"+ userInDb);
+				return "Account already exists, Kindly Login with your Username and Password";
+			}
+			else
+			{
+				
+				userService.saveUser(signin);
+				log.info("user Signed-in Successfully");
+			}
+			return "Account Successfully Signed-In";
 		}
-		else
+		
+		/**
+		 * 
+		 * @param trackerDto
+		 * @return
+		 */
+		@PostMapping("/expenseTracker")
+		public ResponseTrackerDto expenses(@RequestBody TrackerDto trackerDto)
 		{
-			userService.saveUser(signin);
+			ResponseTrackerDto responseDto=new ResponseTrackerDto();
+			Double totalAmount=expenseService.calculateBillAmount(trackerDto.getActivity());
+			
+			User user=new User(trackerDto.getUsername(), trackerDto.getEmail());
+			Integer userPresentInDb=userService.checkCustomerinDb(trackerDto);
+			
+			if(userPresentInDb!=null)
+			{
+				user.setUserId(userPresentInDb);
+				log.info("User already present in Db with userID:"+ userPresentInDb);
+			}
+			else
+			{
+				userService.saveUser(trackerDto);
+				log.info("user saved to Database Successfully");
+			}
+			ExpenseTracker tracker=new ExpenseTracker(user,trackerDto.getActivity());
+			tracker.setTotalAmount(totalAmount);
+			expenseService.save(tracker);
+			log.info("Order placed Successfully");
+			
+			responseDto.setExpenseId(tracker.getExpenseId());
+			responseDto.setTotalAmount(totalAmount);
+			return responseDto;
 		}
-		return "Account Successfully Signed-In";
 	}
 
-	@PostMapping("/expenseTracker")
-	public ResponseTrackerDto expenses(@RequestBody TrackerDto trackerDto)
-	{
-		ResponseTrackerDto responseDto=new ResponseTrackerDto();
-		
-		User user=new User(trackerDto.getUsername(), trackerDto.getPassword());
-		Integer userPresentInDb=userService.checkCustomerinDb(trackerDto);
-		if(userPresentInDb!=null)
-		{
-			user.setUserId(userPresentInDb);
-		}
-		else
-		{
-			userService.saveUser(trackerDto);
-		}
-		Double totalAmount=expenseService.calculateBillAmount(trackerDto);
-		return null;
-	}
-}
+
