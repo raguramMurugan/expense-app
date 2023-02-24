478589abea872aa51bb174a18ca762500999758a
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2023-02-24 21:30:06 +0530

    Updated

---
+++ ResponseErrorHandler.java
@@ -0,0 +1,10 @@
+package com.hubino.expensetrackingapp.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseErrorHandler {
+	
+	private String errorStatus;
+
+}
---
+++ ResponseSuccessHandler.java
@@ -0,0 +1,10 @@
+package com.hubino.expensetrackingapp.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseSuccessHandler {
+	
+	private String successStatus;
+
+}
